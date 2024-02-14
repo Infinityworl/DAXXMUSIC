0578549edf7583128291184a6fd95f2cd793337b
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-02-15 03:52:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,6 +54,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Infinityworl/DAXXMUSIC",
+)    
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
